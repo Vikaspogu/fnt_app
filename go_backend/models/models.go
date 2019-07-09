f8c0e9cdcca5b61028309e148be5f9128716be14
Vikas Pogu <Vikaspoguadf@gmail.com>
2019-07-09 10:24:49 -0400

    omitempty

--- go_backend/models/models.go
+++ go_backend/models/models.go
@@ -34,7 +34,7 @@ type RequestedSocial struct {
 	ID             primitive.ObjectID `bson:"_id" json:"id,omitempty"`
 	Place          string             `json:"place"`
 	Location       string             `json:"location"`
-	Votes          []string           `json:"votes"`
+	Votes          []string           `json:"votes,omitempty"`
 	AdditionalInfo string             `json:"additionalInfo"`
 	CreatedAt      time.Time          `bson:"created_at" json:"created_at,omitempty"`
 }
@@ -45,7 +45,7 @@ type RequestedTalk struct {
 	Topic          string             `json:"topic"`
 	Presenter      string             `json:"presenter"`
 	Date           string             `json:"date"`
-	Votes          []string           `json:"votes"`
+	Votes          []string           `json:"votes,omitempty"`
 	AdditionalInfo string             `json:"additionalInfo"`
 	CreatedAt      time.Time          `bson:"created_at" json:"created_at,omitempty"`
 }
