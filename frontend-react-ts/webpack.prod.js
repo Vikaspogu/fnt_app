4c8d32db92e2aad2ac8a18c8eba763a8e0151118
Vikaspogu <Vikaspoguadf@gmail.com>
2019-08-22 19:44:35 -0400

    Adding webpack prod config
    webpack

--- frontend-react-ts/webpack.prod.js
+++ frontend-react-ts/webpack.prod.js
@@ -25,11 +25,13 @@ module.exports = merge(common, {
         include: [
           path.resolve(__dirname, 'src'),
           path.resolve(__dirname, 'node_modules/patternfly'),
+          path.resolve(__dirname, 'node_modules/react-datepicker/dist'),
           path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-table'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css')
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css')
         ],
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
