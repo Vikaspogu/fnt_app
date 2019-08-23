60bbf004b5cc9d9704898c3ce5fa2a0d9df4ed20
Vikaspogu <Vikaspoguadf@gmail.com>
2019-08-23 09:37:55 -0400

    Update techtalk_dao.go
    bugging mongodb

--- go_backend/dao/techtalk_dao.go
+++ go_backend/dao/techtalk_dao.go
@@ -38,7 +38,7 @@ func init() {
 	}
 	// Collection types can be used to access the database
 	db = client.Database(DBName)
-	fmt.Println("Connection success to Database:: ", db.Name())
+	fmt.Println("Connection success to Database:: ", db.Name(), db.Client())
 }
 
 // InsertOneTalk inserts one item from TechTalk model
