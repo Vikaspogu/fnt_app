f6ba924a6059d514f2864345ef52980790ccbc9f
Vikaspogu <Vikaspoguadf@gmail.com>
2019-08-22 20:23:07 -0400

    webpack
    Updated webpack prod config

--- README.md
+++ README.md
@@ -2,10 +2,36 @@
 
 [![Go Report Card](https://goreportcard.com/badge/vikaspogu/fnt_app)](https://goreportcard.com/report/github.com/vikaspogu/fnt_app)
 
-## Frontend 
+## Frontend
 
 Frontend is developed using ReactJS and Pattenfly as design
 
 ## Golang backend
 
 Go backend using gin gonic, mongo as database
+
+### Deploy to heroku
+
+```bash
+cd ../go_backend
+heroku git:remote -a go-backend-fnt
+git remote rename heroku heroku-backend-app
+cd image-scraper-ts
+heroku git:remote -a image-scraper-fnt
+git remote rename heroku heroku-image-app
+cd ../frontend-react-ts
+heroku git:remote -a frontend-react-fnt
+git remote rename heroku heroku-frontend-app
+cd ../
+git subtree push --prefix frontend-react-ts heroku-frontend-app master
+git subtree push --prefix go_backend heroku-backend-app master
+git subtree push --prefix image-scraper-ts heroku-image-app master
+```
+
+Reset heroku git repo
+
+```bash
+$ cd /my-project/
+$ heroku plugins:install heroku-repo
+$ heroku repo:reset
+```
--- frontend-react-ts/webpack.prod.js
+++ frontend-react-ts/webpack.prod.js
@@ -1,23 +1,10 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({})
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[name].bundle.css'
-    })
-  ],
   module: {
     rules: [
       {
@@ -33,7 +20,7 @@ module.exports = merge(common, {
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css')
         ],
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
